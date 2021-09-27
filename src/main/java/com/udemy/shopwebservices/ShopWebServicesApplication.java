a0d3252c62bb5ecaf9b0348e6b34695e83ec0746
OseiasDomingues <oseiasdomingues9@gmail.com>
2021-09-27 14:47:03 -0300

    Project created

---
+++ src/main/java/com/udemy/shopwebservices/ShopWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.shopwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopWebServicesApplication.class, args);
+	}
+
+}
