7d41154de7ab697a0af4ff56da603ef9dcfd4d53
OseiasDomingues <oseiasdomingues9@gmail.com>
2021-09-28 11:39:01 -0300

    Service layer, component registration

---
+++ src/main/java/com/udemy/shopwebservices/services/UserServices.java
@@ -0,0 +1,26 @@
+package com.udemy.shopwebservices.services;
+
+import com.udemy.shopwebservices.entities.User;
+import com.udemy.shopwebservices.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServices {
+
+    @Autowired
+    UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
