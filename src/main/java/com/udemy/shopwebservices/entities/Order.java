6d2c6d2aa2302d6e836292b678afde9401d97493
OseiasDomingues <oseiasdomingues9@gmail.com>
2021-09-29 10:59:39 -0300

    OrderStatus enum

--- src/main/java/com/udemy/shopwebservices/entities/Order.java
+++ src/main/java/com/udemy/shopwebservices/entities/Order.java
@@ -1,13 +1,14 @@
 package com.udemy.shopwebservices.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.udemy.shopwebservices.entities.enums.OrderStatus;
 import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
 
-@AllArgsConstructor
+
 @NoArgsConstructor
 @Getter
 @Setter
@@ -23,10 +24,28 @@ public class Order implements Serializable  {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
 
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
 
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
 }
