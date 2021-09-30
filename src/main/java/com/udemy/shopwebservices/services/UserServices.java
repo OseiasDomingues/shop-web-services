82800139ab8c7e91dceb0e53b0212f4463b6e0d8
OseiasDomingues <oseiasdomingues9@gmail.com>
2021-09-30 15:02:05 -0300

    Exception handling - update

--- src/main/java/com/udemy/shopwebservices/services/UserServices.java
+++ src/main/java/com/udemy/shopwebservices/services/UserServices.java
@@ -2,8 +2,11 @@ package com.udemy.shopwebservices.services;
 
 import com.udemy.shopwebservices.entities.User;
 import com.udemy.shopwebservices.repositories.UserRepository;
+import com.udemy.shopwebservices.services.exceptions.DatabaseException;
 import com.udemy.shopwebservices.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.ResourceAccessException;
 
@@ -31,7 +34,13 @@ public class UserServices {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
